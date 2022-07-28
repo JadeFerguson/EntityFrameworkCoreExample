1fa653a18733e6f73570bf1c6c71ba2a84bba427
Jade Ferguson <jadesnider2440@gmail.com>
2022-07-28 14:21:14 -0700

    Created a student model to track in the database

---
+++ EntityFrameworkCoreExample/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCoreExample
+{
+    public class Student
+    {
+        [Key] // will mark as primary key
+        public int Id { get; set; } // will assume primary key because of ID property
+
+        public string FullName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
